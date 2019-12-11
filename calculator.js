ca1a9aab1e68d55259f2b4a3482b8c7cebcac5e9
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-11 16:03:48 +0100

    calculator.js
    Added another event

--- calculator.js
+++ calculator.js
@@ -1,10 +1,16 @@
-var n1 = document.querySelector('#number1');
-var n2 = document.querySelector('#number2');
-var plus = document.querySelector('#plus');
+let n1 = document.querySelector('#number1');
+let n2 = document.querySelector('#number2');
+let plus = document.querySelector('#plus');
+let calculate = document.querySelector('#calculate');
+let total = document.querySelector('#total');
 
 plus.addEventListener('click', ()=>{
   var sum = n1 + n2;
   return sum;
+});
+
+calculate.addEventListener('click', ()=>{
+    console.log('Ciao');
 });
 
 console.log('Calculator');
