f15be85714a0be6659a2e25709bed076e9b1afa7
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-11 15:55:37 +0100

    calculator.js
    Added sum function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,10 @@
+var n1 = document.querySelector('#number1');
+var n2 = document.querySelector('#number2');
+
+function sum(n1, n2){
+  total = n1 + n2;
+  return total;
+}
+
 console.log('Calculator');
+console.log('Sharktale');
