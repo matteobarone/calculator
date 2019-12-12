e1fcb0579a203708ed260b3639249e76725e39bc
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-12 10:14:35 +0100

    edited calculate button

--- calculator.js
+++ calculator.js
@@ -1,17 +1,8 @@
-let n1 = document.querySelector('#number1');
-let n2 = document.querySelector('#number2');
-let plus = document.querySelector('#plus');
-let calculate = document.querySelector('#calculate');
-let total = document.querySelector('#total');
 
-plus.addEventListener('click', (n1, n2)=>{
-  var sum = n1 + n2;
-  return sum;
-});
+let calcula = document.querySelector('#calculate');
 
-calculate.addEventListener('click', ()=>{
-    console.log('Ciao');
+calcula.addEventListener('click', ()=>{
+    alert('Ciao');
 });
 
 console.log('Calculator');
-console.log('Sharktale');
