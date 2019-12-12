06fd3302ead526d81ac9b0356954439aa29764fe
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-12 11:54:01 +0100

    added full version of calculator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,45 @@
+document.addEventListener('DOMContentLoaded', ()=>{
 
-let calcula = document.querySelector('#calculate');
+    function add() {
+        var x = Number(document.getElementById('number1').value);
+        var y = Number(document.getElementById('number2').value);
 
-calcula.addEventListener('click', ()=>{
-    alert('Ciao');
+        document.getElementById('total').innerHTML = x + y;
+        return false;
+    }
+
+    function subtract() {
+        var x = Number(document.getElementById('number1').value);
+        var y = Number(document.getElementById('number2').value);
+
+        document.getElementById('total').innerHTML = x - y;
+        return false;
+    }
+
+    function multiply() {
+        var x = Number(document.getElementById('number1').value);
+        var y = Number(document.getElementById('number2').value);
+
+        document.getElementById('total').innerHTML = x * y;
+        return false;
+    }
+
+    function divide() {
+        var x = Number(document.getElementById('number1').value);
+        var y = Number(document.getElementById('number2').value);
+
+        document.getElementById('total').innerHTML = x % y;
+        return false;
+    }
+
+    var el0 = document.getElementById('plus');
+    var el1 = document.getElementById('minus');
+    var el2 = document.getElementById('multi');
+    var el3 = document.getElementById('divide');
+
+    el0.addEventListener('click', add);
+    el1.addEventListener('click', subtract);
+    el2.addEventListener('click', multiply);
+    el3.addEventListener('click', divide);
+
 });
-
-console.log('Calculator');
