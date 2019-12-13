ff6f04da4c8ba408d052e23de73683d11eeab770
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-13 14:14:10 +0100

    changes

--- calculator.js
+++ calculator.js
@@ -2,26 +2,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     function calc(){
 
-        var x = parseFloat(document.getElementById('number1').value);
-        var y = parseFloat(document.getElementById('number2').value);            
-        var opr = document.getElementsByClassName('operators').value;
+        var x = parseFloat(document.getElementById('#number1').value);
+        var y = parseFloat(document.getElementById('#number2').value);            
+        var opr = document.getElementsByClassName('.operators').value;
 
         if(opr === "plus"){
-            var el1 = document.getElementById('plus');
-            el1.addEventListener('click', ()=>{
-                document.getElementById('total').innerHTML = x + y;
+            var el1 = document.getElementById('#plus').addEventListener( 'click', () => {
+                document.getElementById('#total').innerHTML = x + y;
             });
         }
         else if(opr === "minus"){
-            document.getElementById('total').innerHTML = x - y;
+            var el2 = document.getElementById('#minus').addEventListener( 'click', () => {
+                document.getElementById('#total').innerHTML = x - y;
+            });
         }
         else if(opr === "multi"){
-            document.getElementById('total').innerHTML = x * y;
+            var el3 = document.getElementById('#multi').addEventListener( 'click', () => {
+                document.getElementById('#total').innerHTML = x * y;
+            });
         }
         else if(opr === "divide"){
-            document.getElementById('total').innerHTML = x % y;
+            var el4 = document.getElementById('#divide').addEventListener( 'click', () => {
+                document.getElementById('#total').innerHTML = x % y;
+            });
         }
 
         return false;
     }
+
+    calc();
 });
