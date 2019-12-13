84189ab1ef1386299072043acc210d7b1aa3e6c2
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-13 15:28:34 +0100

    removed "#"

--- calculator.js
+++ calculator.js
@@ -2,28 +2,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     function calc(){
 
-        var x = parseFloat(document.getElementById('#number1').value);
-        var y = parseFloat(document.getElementById('#number2').value);            
-        var opr = document.getElementsByClassName('.operators').value;
+        var x = parseFloat(document.getElementById('number1').value);
+        var y = parseFloat(document.getElementById('number2').value);            
+        var opr = document.getElementsByClassName('operators').value;
 
         if(opr === "plus"){
-            var el1 = document.getElementById('#plus').addEventListener( 'click', () => {
-                document.getElementById('#total').innerHTML = x + y;
+            var el1 = document.getElementById('plus').addEventListener( 'click', () => {
+                document.getElementById('total').innerHTML = x + y;
             });
         }
         else if(opr === "minus"){
-            var el2 = document.getElementById('#minus').addEventListener( 'click', () => {
-                document.getElementById('#total').innerHTML = x - y;
+            var el2 = document.getElementById('minus').addEventListener( 'click', () => {
+                document.getElementById('total').innerHTML = x - y;
             });
         }
         else if(opr === "multi"){
-            var el3 = document.getElementById('#multi').addEventListener( 'click', () => {
-                document.getElementById('#total').innerHTML = x * y;
+            var el3 = document.getElementById('multi').addEventListener( 'click', () => {
+                document.getElementById('total').innerHTML = x * y;
             });
         }
         else if(opr === "divide"){
-            var el4 = document.getElementById('#divide').addEventListener( 'click', () => {
-                document.getElementById('#total').innerHTML = x % y;
+            var el4 = document.getElementById('divide').addEventListener( 'click', () => {
+                document.getElementById('total').innerHTML = x % y;
             });
         }
 
