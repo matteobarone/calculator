6990891e528be5f0a71fae7af62b153d15777535
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-15 21:21:51 +0100

    -

--- calculator.js
+++ calculator.js
@@ -1,27 +1,37 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+        
+        calculate.addEventListener('click', () => {
+
+            let x = parseInt(number1.value);
+            let y = parseInt(number2.value);
+
+            console.log("Number 1 value is:" + " " + x);
+            console.log("Number 2 value is:" + " " + y);    
+
+        });
     
         plus.addEventListener( 'click', function(){
-            x = number1.value;
-            y = number2.value; 
+            let x = number1.value;
+            let y = number2.value; 
             document.getElementById('total').innerHTML = (+x)+(+y);
         });
 
         minus.addEventListener( 'click', function(){
-            x = number1.value;
-            y = number2.value;
+            let x = number1.value;
+            let y = number2.value;
             document.getElementById('total').innerHTML = (+x)-(+y);
         });
 
         multi.addEventListener( 'click', function(){
-            x = number1.value;
-            y = number2.value;
+            let x = number1.value;
+            let y = number2.value;
             document.getElementById('total').innerHTML = x * y;
         });
 
         divide.addEventListener( 'click', function(){
-            x = number1.value;
-            y = number2.value;
-            res = x / y;
+            let x = number1.value;
+            let y = number2.value;
+            let res = x / y;
             document.getElementById('total').innerHTML = res.toFixed(3);
         });
     
