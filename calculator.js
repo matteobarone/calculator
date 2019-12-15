232a9f7400a11581e044edc7210c22268c28968f
Manuel Alonge <48571023+manuelalonge@users.noreply.github.com>
2019-12-15 21:17:46 +0100

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,34 +1,28 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    
+        plus.addEventListener( 'click', function(){
+            x = number1.value;
+            y = number2.value; 
+            document.getElementById('total').innerHTML = (+x)+(+y);
+        });
 
-    function calc(){
+        minus.addEventListener( 'click', function(){
+            x = number1.value;
+            y = number2.value;
+            document.getElementById('total').innerHTML = (+x)-(+y);
+        });
 
-        var x = parseFloat(document.getElementById('number1').value);
-        var y = parseFloat(document.getElementById('number2').value);            
-        var opr = document.getElementsByClassName('operators').value;
+        multi.addEventListener( 'click', function(){
+            x = number1.value;
+            y = number2.value;
+            document.getElementById('total').innerHTML = x * y;
+        });
 
-        if(opr === "plus"){
-            var el1 = document.getElementById('plus').addEventListener( 'click', () => {
-                document.getElementById('total').innerHTML = x + y;
-            });
-        }
-        else if(opr === "minus"){
-            var el2 = document.getElementById('minus').addEventListener( 'click', () => {
-                document.getElementById('total').innerHTML = x - y;
-            });
-        }
-        else if(opr === "multi"){
-            var el3 = document.getElementById('multi').addEventListener( 'click', () => {
-                document.getElementById('total').innerHTML = x * y;
-            });
-        }
-        else if(opr === "divide"){
-            var el4 = document.getElementById('divide').addEventListener( 'click', () => {
-                document.getElementById('total').innerHTML = x % y;
-            });
-        }
-
-        return false;
-    }
-
-    calc();
+        divide.addEventListener( 'click', function(){
+            x = number1.value;
+            y = number2.value;
+            res = x / y;
+            document.getElementById('total').innerHTML = res.toFixed(3);
+        });
+    
 });
